f4ebb5cc526d1c296b1c835e22aada31d78d101e
Rohit N <rohitnair@MacBook-Airy.local>
2024-03-26 15:00:32 -0500

    Initial auth setup

---
+++ app/register/page.tsx
@@ -0,0 +1,13 @@
+import Form from "./form";
+import {getServerSession} from "next-auth";
+import {redirect} from "next/navigation";
+
+export default async function RegisterPage() {
+    const session = await getServerSession();
+    if (session) {
+        redirect("/");
+    }
+    return (
+        <Form />
+    )
+}

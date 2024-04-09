f35b54e4691bbf635c73e19290b2c7a2ee236217
Abdullah Bayindir <byndrabdullah@gmail.com>
2024-04-09 11:47:24 -0500

    combining profile pages

--- app/page.js
+++ app/page.js
@@ -6,6 +6,10 @@ export default function Home() {
     <main className="min-h-screen items-center justify-between p-8">
       <h1>Cause Way</h1>
       <Link href="non-profit-profile" className="text-blue-600">Non profit profile prototype</Link>
+      <br/>
+      <Link href="person_profile" className="text-blue-600">person profile</Link>
+      <br/>
+      <Link href="company-profile" className="text-blue-600">company profile</Link>
     </main>
   );
 }

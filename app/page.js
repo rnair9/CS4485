ca19ebfe3e6509e113df6dbc13aa5808d6d41052
aoa190007 <aoa190007@utdallas.edu>
2024-04-16 11:17:33 -0500

    added an about me

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ export default function Home() {
     <main className="min-h-screen items-center justify-between p-8">
       <h1>Cause Way</h1>
       <Link href="person_profile" className="min-h-screen items-center justify-between p-8 text-blue-600">Person profile prototype</Link>
+      <Link href="About_us" className="min-h-screen items-center justify-between p-8 text-blue-600">About Us</Link>
     </main>
   );
 }

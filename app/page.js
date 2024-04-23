9b641b65c1f5a0d1be1b28cc4295918fed78b006
aoa190007 <aoa190007@utdallas.edu>
2024-04-23 13:19:26 -0500

    new donation page and volunteer sign-up page

--- app/page.js
+++ app/page.js
@@ -7,6 +7,8 @@ export default function Home() {
       <h1>Cause Way</h1>
       <Link href="person_profile" className="min-h-screen items-center justify-between p-8 text-blue-600">Person profile prototype</Link>
       <Link href="About_us" className="min-h-screen items-center justify-between p-8 text-blue-600">About Us</Link>
+      <Link href="donation" className="min-h-screen items-center justify-between p-8 text-blue-600">Donation</Link>
+      <Link href="volunteer_signup" className="min-h-screen items-center justify-between p-8 text-blue-600">Volunteer Sign Up</Link>
     </main>
   );
 }

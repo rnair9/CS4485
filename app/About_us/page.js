ae0677abe7baa6d0a124c4a0161e6cc891d63ddc
aoa190007 <aoa190007@utdallas.edu>
2024-04-23 14:24:50 -0500

    fix lint

--- app/About_us/page.js
+++ app/About_us/page.js
@@ -3,14 +3,10 @@ export default function profile() {
         <div>
             <form class="m-4 text-center">
                 <h1 className="font-bold container text-4xl p-8"> ABOUT CAUSEWAY</h1>
-                <p>Welcome to our platform dedicated to fostering collaboration between non-profit organizations, companies, and individuals to drive positive social change!
-                    Causeway is an application that is dedicated to creating meaningful connections that empower communities and make a difference in the world.
-                    As Causeway of this innovative platform, we are committed to ensuring that every user's experience is seamless, efficient, and impactful.
-                    Here's a glimpse into what you can expect from our platform:
-                    </p>
-                    </form>
+                <p>Welcome to our platform dedicated to fostering collaboration between non-profit organizations, companies, and individuals to drive positive social change! Causeway is an application that is dedicated to creating meaningful connections that empower communities and make a difference in the world. As Causeway of this innovative platform, we are committed to ensuring that the user experience is seamless, efficient, and impactful. Here is a glimpse into what you can expect from our platform:</p>
+            </form>
             <div class = "m-6 space-y-4">
-                    <p><strong>Profiles for Everyone:</strong>Whether you're a company, a non-profit organization, or a volunteer, you can create a personalized profile that showcases your mission, values, and impact.</p>
+                    <p><strong>Profiles for Everyone:</strong>Whether you&apos;re a company, a non-profit organization, or a volunteer, you can create a personalized profile that showcases your mission, values, and impact.</p>
                     <p><strong>Grant Opportunities:</strong>Companies have the opportunity to post grant applications, providing non-profits with vital funding to support their initiatives. Non-profits can easily apply for grants after reviewing available opportunities.</p>
                     <p><strong>Transparent Donation Process:</strong>Our platform facilitates transparent and secure donations from companies and individuals to non-profit organizations. Donors can track their donation history and receive automated email receipts for their contributions.</p>
                     <p><strong>Volunteer Opportunities:</strong>Companies and individuals can sign up to volunteer for non-profit organizations, offering their time and skills to support important causes in their communities.</p>

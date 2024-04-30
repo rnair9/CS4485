d194da509ede618a7a6ea56088e3a6dd77cc606e
Abdullah Bayindir <byndrabdullah@gmail.com>
2024-04-30 15:47:29 -0500

    non profits can create posts that will show on the home page

--- app/page.js
+++ app/page.js
@@ -1,51 +1,30 @@
+"use client";
 import Link from "next/link";
 import Post from "../components/postComponent/Post";
+import { useState, useEffect } from "react";
+import { getSession } from "next-auth/react";
 
-export default async function Home() {
-  // console.log(session)
-  const posts = [
-    {
-      "id": 1,
-      "title": "Post 1",
-      "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      "image": "https://www.placeofhope.com/wp-content/uploads/woocommerce-placeholder.png",
-      "owner": "Organization A"
-    },
-    {
-      "id": 2,
-      "title": "Post 2",
-      "desc": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      "image": "https://www.placeofhope.com/wp-content/uploads/woocommerce-placeholder.png",
-      "owner": "Organization B"
-    },
-    {
-      "id": 3,
-      "title": "Post 3",
-      "desc": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-      "image": "https://www.placeofhope.com/wp-content/uploads/woocommerce-placeholder.png",
-      "owner": "Organization C"
-    },
-    {
-      "id": 4,
-      "title": "Post 4",
-      "desc": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      "image": "https://www.placeofhope.com/wp-content/uploads/woocommerce-placeholder.png",
-      "owner": "Organization D"
-    },
-    {
-      "id": 5,
-      "title": "Post 5",
-      "desc": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
-      "image": "https://www.placeofhope.com/wp-content/uploads/woocommerce-placeholder.png",
-      "owner": "Organization E"
-    }
-  ];
+
+export default function Home() {
+  const [posts, setPosts] = useState([])
+  const session = getSession()
+
+  const fetchPosts = async () => {
+    const response = await fetch(`api/createPost/donation`);
+    const data = await response.json();
+    // console.log(data.posts)
+    setPosts(data.posts)
+}
+
+useEffect(() => {
+  fetchPosts()
+});
   return (
     <main className="min-h-screen items-center justify-between">
       <div className="flex justify-center">
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
-          {posts.map((post, index) => (
-            <Post post={post} key={post.id} />
+          {posts.toReversed().map((post, index) => (
+            <Post post={post} key={post.initiativeid} />
           ))}
         </div>
       </div>
--- components/postComponent/Post.js
+++ components/postComponent/Post.js
@@ -6,11 +6,11 @@ function Post({ post, sharedBy }) {
   // console.log(decodeURIComponent(post.title))
   return (
     <div className="flex justify-center">
-      <div className="max-w-md mx-auto">
+      <div className="max-w-md min-w-80 mx-auto">
         <div className="bg-gray-800 shadow-lg rounded-lg overflow-hidden my-10">
           <div className="relative h-64">
             <Image 
-              src={post.image} 
+              src="https://www.placeofhope.com/wp-content/uploads/woocommerce-placeholder.png"
               layout="fill" 
               objectFit="cover" 
               alt={post.title} 
@@ -18,16 +18,17 @@ function Post({ post, sharedBy }) {
             />
           </div>
           <div className="px-6 py-4">
-            <h2 className="text-white text-xl font-bold mb-2">{post.title}</h2>
-            <p className="text-gray-300 text-base">{post.desc}</p>
+            <h2 className="text-white text-xl font-bold mb-2">{post.name}</h2>
+            <p className="text-gray-400 text-sm mt-2">{post.category}</p>
+            <p className="text-gray-300 text-base">{post.description}</p>
             {sharedBy && (
               <p className="text-gray-400 text-sm mt-2">
-                Shared by: {sharedBy}
+                Shared by: {post.nonprofitid}
               </p>
             )}
           </div>
           <div className="px-6 py-4 flex justify-center">
-            <Link href={"/donation/" + post.id} passHref className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-full transition duration-300">
+            <Link href={"/donation/" + post.category} passHref className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-full transition duration-300">
                 Donate Now
             </Link>
           </div>

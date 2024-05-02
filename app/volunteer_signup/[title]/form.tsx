53bcccac76afad5b0c7d8199590df5b29961e7fd
Abdullah Bayindir <byndrabdullah@gmail.com>
2024-05-02 12:52:26 -0500

    volunteer email on form submit

---
+++ app/volunteer_signup/[title]/form.tsx
@@ -0,0 +1,93 @@
+"use client";
+import { useState } from "react";
+import sendEmail from "../../email-utility";
+
+export default function Form() {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNumber: '',
+});
+
+const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    setFormData({
+        ...formData,
+        [id]: value
+    });
+};
+
+const handleSendEmail = async (e) => {
+    e.preventDefault()
+    // console.log(formData.firstName)
+    await sendEmail(`${formData.email}`, `Volunteer Sign-up`, ` <div className="p-8 bg-gray-100">
+    <div className="max-w-xl mx-auto bg-white p-8 rounded-lg shadow-md">
+      <p className="mb-4">Dear ${formData.firstName} ${formData.lastName},</p>
+      <h1 className="text-3xl font-bold mb-4">Thank You for Volunteering!</h1>
+      <p className="mb-4">We want to express our sincere gratitude for your willingness to volunteer your time and efforts.</p>
+      <p className="mb-4">Thank you for stepping forward to support the cause; your dedication is invaluable to the nonprofit's mission.</p>
+      <p className="mb-4">Thank you once again for your kindness and commitment.</p>
+      <p className="mb-4">Best regards,</p>
+      <p>CauseWay Team</p>
+    </div>
+  </div>`);
+    alert("Donation processed, and email sent for confirmation")
+    // router.push("/")
+  };
+  return (
+    <div>
+      <form className="m-4 text-center">
+        <h1 className="font-bold text-4xl p-4 flex justify-center">
+          VOLUNTEER SIGN-UP
+        </h1>
+        {/* <p>display non-profit info and the event info at the top</p> */}
+        <h2 className="text-left container text-3xl p-2">
+          Volunteer Information
+        </h2>
+        <div className="grid gap-6 mb-6 md:grid-cols-4">
+          <input
+            type="text"
+            id="firstName"
+            className="white border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="First Name"
+            required
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            id="lastName"
+            className="white border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Last Name"
+            required
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            id="email"
+            className="white border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Email"
+            required
+            onChange={handleInputChange}
+          />
+          <input
+            type="tel"
+            pattern="[0-9]{3} [0-9]{3} [0-9]{4}" maxLength={12}
+            id="phone_num"
+            className="white border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Phone Number: 888 888 8888"
+            required
+            onChange={handleInputChange}
+          />
+        </div>
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          onClick={handleSendEmail}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
